36544e560136cd093d6baa31bfa3db41248b7de4
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-04-08 21:02:20 +0200

    Added lenghth checker for the password

--- src/main/java/PassWordChecker.java
+++ src/main/java/PassWordChecker.java
@@ -4,7 +4,19 @@
 
 class PassWordChecker{
     public static void main(String[] args){
+        boolean isPassword = isPasswordValid("jhvcihjs");
+        if(isPassword == true) System.out.println("The password is valid");
+        
+        System.out.println(isPassword);
 
-     
+    
+        
     }
+  static boolean  isPasswordValid(String password){
+    if(password.length() < 8){
+        return  false;
+    }
+      
+
+  }
 }
