087e2559ddeaf26512c079d5c55123e27d8f82c5
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2025-04-08 21:26:59 +0200

    Added type check for each letter in a string

--- src/main/java/PassWordChecker.java
+++ src/main/java/PassWordChecker.java
@@ -1,22 +1,46 @@
+package main.java;
 
-
-
-
+/**
+ * This is a password checker class , 
+ * @How it works: it checks the length first if its less than 8 chars then return false
+ * Loop on each charecter on on the password and check the types
+ */
 class PassWordChecker{
     public static void main(String[] args){
-        boolean isPassword = isPasswordValid("jhvcihjs");
+        boolean isPassword = isPasswordValid("MAND56@#@$jhll");
         if(isPassword == true) System.out.println("The password is valid");
-        
-        System.out.println(isPassword);
 
-    
-        
+        System.out.println(isPassword);  
     }
   static boolean  isPasswordValid(String password){
     if(password.length() < 8){
-        return  false;
-    }
-      
+        return  false; // checking the length
+    }else{
+     boolean hasLowercase = false;
+    boolean hasUpper = false;
+    boolean hasSpecialChar = false;
+    boolean hasNumber = false;
 
+    // looping the password chars ..
+    for(int i = 0 ; i < password.length(); i ++){
+        char letter = password.charAt(i);
+
+    if(Character.isUpperCase(letter)){
+        hasUpper = true;
+    }else if(Character.isLowerCase(letter)){
+        hasLowercase = false;
+    }else if(Character.isDigit(letter)){
+        hasNumber = true;
+    }else if(!Character.isLetterOrDigit(letter)){
+        hasSpecialChar = true;
+    }
+    }
+    if(hasLowercase && hasNumber && hasSpecialChar && hasUpper){
+        return  true;
+    }
+      return  false;
+    }
+   
   }
+
 }
